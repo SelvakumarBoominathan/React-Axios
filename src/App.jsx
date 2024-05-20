fe78d36cbc46544fb65add8908879e1ff7c1b535
Selvakumar Boominathan <selvans2k@gmail.com>
2024-05-21 01:07:56 +0530

    Card styles added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,41 @@
 // import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+import axios from 'axios';
 import Header from "./Components/Header/Header.jsx";
-import Content from "./Components/Content/Content.jsx"
+import Content from "./Components/Content/Content.jsx";
+import { useEffect,useState } from 'react';
 
 function App() {
+const [data, setData] = useState([]);
+
+useEffect(()=>{
+
+  const fetchData = async() =>{
+    try {
+      const response = await axios.get('https://664b818e35bbda10987d2f52.mockapi.io/Employee');
+      setData(response.data)
+      console.log(data)
+    } catch (error) {
+      console.error('Error message: ', error)
+    }
+  }
+
+  fetchData();
+}, [])
+
+
+  const handleEdit = () => {
+    console.log('Edit button clicked');
+  };
+
+  const handleDelete = () => {
+    console.log('Delete button clicked');
+  };
 
   return (
     <div>
       <Header/>
-      <Content/>
+      <Content Data={data} onDelete={handleDelete} onEdit={handleEdit} />
     </div>
   )
 }
--- src/Components/Content/Content.jsx
+++ src/Components/Content/Content.jsx
@@ -3,22 +3,32 @@ import "./Content.css"
 
 const Content = ({Data, onEdit, onDelete}) => {
 
+
+  // to check if the fetched data is valid in UI
+  if (!Data) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className='Parent-content-container'>
-      {Data.map((Emp, index) => (
+
+      
+
+    {/* Map function to pass the individual details of an employee from data array */}
+      {Data.map((emp, index) => (
   <div className="card" key={index}>
   
-      <img src={Emp.image} alt="User" className="card-image" />
+      <img src={emp.Picture} alt="User" className="card-image" />
       <div className="card-details">
-        <h2>{Emp.username}</h2>
-        <p>Email: {Emp.email}</p>
-        <p>Country: {Emp.country}</p>
-        <p>Age: {Emp.age}</p>
+        <h2>{emp.UserName}</h2>
+        <p>Email: {emp.Email}</p>
+        <p>Country: {emp.Country}</p>
+        <p>Age: {emp.Age}</p>
       </div>
       
       <div className="card-buttons">
-        <button onClick={onEdit} className="edit-button">Edit</button>
-        <button onClick={onDelete} className="delete-button">Delete</button>
+        <button onClick={() => onEdit(emp)} className="edit-button">Edit</button>
+        <button onClick={() => onDelete(emp)} className="delete-button">Delete</button>
       </div>
     
     </div>
