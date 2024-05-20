9fb35797f8d24e77c19f9c95ad2167ca3d65dec6
Selvakumar Boominathan <selvans2k@gmail.com>
2024-05-20 23:12:03 +0530

    Added styles and alignment for card

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 // import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
-import Header from "./Components/Header/Header.jsx"
+import Header from "./Components/Header/Header.jsx";
+import Content from "./Components/Content/Content.jsx"
 
 function App() {
 
   return (
     <div>
       <Header/>
+      <Content/>
     </div>
   )
 }
--- src/Components/Content/Content.jsx
+++ src/Components/Content/Content.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import "./Content.css"
 
-const Content = () => {
+const Content = (props) => {
+  
+  //props deStructuring
+  const {image, username, email, country, age, onEdit, onDelete } = props;
+
   return (
-    <div>
+    <div className='Parent-content-container'>
+  <div className="card">
+      <img src={image} alt="User" className="card-image" />
+      <div className="card-details">
+        <h2>{username}</h2>
+        <p>Email: {email}</p>
+        <p>Country: {country}</p>
+        <p>Age: {age}</p>
+      </div>
+      <div className="card-buttons">
+        <button onClick={onEdit} className="edit-button">Edit</button>
+        <button onClick={onDelete} className="delete-button">Delete</button>
+      </div>
+    </div>
       
     </div>
   )
 }
 
-export default Content
+export default Content;
